419d967297323a3d29d75a0c393c5e7827b26f97
Carlos Daniel Taveras Liranzo <20212021@itla.edu.do>
2023-07-24 23:14:48 -0400

    Filas y columnas from Category

--- BulkyBook.Web/Controllers/CategoryController.cs
+++ BulkyBook.Web/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBook.Web.Data;
+using BulkyBook.Web.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategories = _db.Categories;
+            return View(objCategories);
         }
     }
 }
